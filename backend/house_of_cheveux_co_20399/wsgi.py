eb85d1f464c77cd4b3041b0df36274d6afe316d6
Crowdbotics <team@crowdbotics.com>
2020-09-17 23:27:25 +0000

    Update models

--- backend/house_of_cheveux_co_20399/wsgi.py
+++ backend/house_of_cheveux_co_20399/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'house_of_cheveux_co_20399.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "house_of_cheveux_co_20399.settings")
 
 application = get_wsgi_application()
